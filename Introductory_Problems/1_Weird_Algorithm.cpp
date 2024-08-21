a38ba21f19a2214874311f200bf4bd87bf70354f
Nithin1729S <sureshnithin1729@gmail.com>
2024-08-22 01:35:49 +0530

    initial commit

---
+++ Introductory_Problems/1_Weird_Algorithm.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll long long int
+#define vi vector<int>
+#define vll vector<long long int>
+#define INF INT_MAX
+#define MOD 1000000007
+#define pii pair<int,int>
+#define endl '\n'
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define clr(x) memset(x,0,sizeof(x))
+#define sortUni(v) sort(all(v)), v.erase(unique(all(v)), v.end())
+#define fast_io ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define test int t;cin>>t;while(t--)
+#define take(a,n) vi a(n); f0(i,n) cin >> a[i];
+#define give(a,n) f0(i,n){cout << a[i] << ' ';}cout << endl;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    cout<<n<<" ";
+    while(n!=1){
+        if(n%2==0){
+            n=n/2;
+            cout<<n<<" ";
+        }else{
+            n=3*n+1;
+            cout<<n<<" ";
+        }
+    }
+
+}
+int32_t main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt","r",stdin);
+        freopen("out.txt","w",stdout);
+    #endif
+    fast_io;
+    //test
+        solve();
+    return 0;
+}
+
---
+++ Introductory_Problems/2_MissingNumber.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+//#define int long long 
+#define ll long long int
+#define vi vector<int>
+#define vll vector<long long int>
+#define INF INT_MAX
+#define MOD 1000000007
+#define pii pair<int,int>
+#define endl '\n'
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define clr(x) memset(x,0,sizeof(x))
+#define sortUni(v) sort(all(v)), v.erase(unique(all(v)), v.end())
+#define fast_io ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define test int t;cin>>t;while(t--)
+#define take(a,n) vi a(n); f0(i,n) cin >> a[i];
+#define give(a,n) f0(i,n){cout << a[i] << ' ';}cout << endl;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<int>vis(n+1,0);
+    for(int i=0;i<n-1;i++){
+        int ele;
+        cin>>ele;
+        vis[ele]=1;
+    }
+    //give(vis,vis.size());
+    // for(int i=0;i<n;i++){
+    //     cout<<vis[i]<<endl;
+    // }
+    for(int i=1;i<=n;i++){
+        if(vis[i]==0){
+            cout<<i<<endl;
+            break;
+        }
+    }
+}
+int32_t main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt","r",stdin);
+        freopen("out.txt","w",stdout);
+    #endif
+    fast_io;
+    //test
+        solve();
+    return 0;
+}
+
