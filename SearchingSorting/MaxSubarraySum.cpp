18f73ae2b88159a9d3e8ae3145f1e942b066ef4e
Nithin1729S <sureshnithin1729@gmail.com>
2025-01-03 00:16:41 +0530

    kadane

---
+++ SearchingSorting/MaxSubarraySum.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define vi vector<int>
+#define vll vector<long long int>
+#define INF INT_MAX
+#define MOD 1000000007
+#define pii pair<int,int>
+#define endl '\n'
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define clr(x) memset(x,0,sizeof(x))
+#define sortUni(v) sort(all(v)), v.erase(unique(all(v)), v.end())
+#define fast_io ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define test int t;cin>>t;while(t--)
+#define f0(i,n) for(int i = 0; i < n; i++)
+#define take(a,n) vi a(n); f0(i,n) cin >> a[i];
+#define give(a,n) f0(i,n){cout << a[i] << ' ';}cout << endl;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<long long>arr;
+    for(int i=0;i<n;i++){
+        long long ele;
+        cin>>ele;
+        arr.push_back(ele);
+    }
+    long long sum=0;
+    long long maxi=-1e18;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        maxi=max(maxi,sum);
+        if(sum<0){
+            sum=0;
+        }
+    }
+    cout<<maxi<<endl;
+
+
+}
+int32_t main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt","r",stdin);
+        freopen("out.txt","w",stdout);
+    #endif
+    fast_io;
+    //test
+        solve();
+    return 0;
+}
+
